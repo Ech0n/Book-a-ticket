08a196b68eae2c2fdd5f9a02012491db8529611f
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-07-14 20:45:57 +0200

    Add basic User model

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-await db.sequelize.sync({ alter: true });
+
+await db.sequelize.sync({ alter: true, logging: false });
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
