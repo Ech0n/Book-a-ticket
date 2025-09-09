caaf78432754047818de5a782349184627d37048
Mateusz <matid@spoko.pl>
2025-09-09 19:29:49 +0200

    [Frontend] Filled About Page

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -1,6 +1,9 @@
 function About() {
-    return <div className="p-15 text-2xl ">
-        <h2>About Page</h2>
+    return <div className="p-15  ">
+        <h1 className="p-15 text-5xl text-center">About</h1>
+        <p className="p-15 text-3xl mx-100">
+            We created our online ticketing platform to make buying tickets easier, faster, and more secure. Our mission is to connect people with the events they love by offering a seamless digital experience – from browsing upcoming shows to receiving instant e-tickets. With a user-friendly design, multiple payment options, and reliable security, we ensure that every step of the journey is simple and stress-free. Whether you’re an event organizer looking to reach more audiences or a customer searching for your next experience, our platform is here to bring convenience and excitement to ticketing.
+        </p>
     </div>;
 }
 
