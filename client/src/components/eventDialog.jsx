d57b926d4a71bd8ca74dea7ce458358c76b22871
Mateusz <matid@spoko.pl>
2025-09-15 08:57:27 +0200

    [Frontend] fixed dialog

--- client/src/components/eventDialog.jsx
+++ client/src/components/eventDialog.jsx
@@ -83,84 +83,83 @@ function EventDialog({
         }
     }
 
-    if (confirmAction) {
-        return (
-            <Dialog open={isDialogOpen} onOpenChange={handleDialogClose}>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>
-                            {confirmAction === "buy"
-                                ? "Confirm Ticket Purchase"
-                                : "Confirm Ticket Return"}
-                        </DialogTitle>
-                        <DialogDescription>
-                            {confirmAction === "buy"
-                                ? `Are you sure you want to buy a ticket for "${event.name}"?`
-                                : `Are you sure you want to return your ticket for "${event.name}"?`}
-                        </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter className="flex justify-end gap-4">
-                        <Button
-                            variant="outline"
-                            onClick={() => setConfirmAction(null)}
-                        >
-                            Cancel
-                        </Button>
-                        <Button onClick={handleConfirm}>Confirm</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-        );
-    }
 
     return (
-        <Dialog open={externalIsOpen !== undefined ? isDialogOpen : undefined} onOpenChange={externalIsOpen !== undefined ? handleDialogClose : undefined} >
+        <Dialog open={confirmAction !== null} onOpenChange={() => setConfirmAction(null)}>
+            <DialogTrigger asChild>
+                <Dialog open={externalIsOpen !== undefined ? isDialogOpen : undefined} onOpenChange={externalIsOpen !== undefined ? handleDialogClose : undefined} >
 
-            {
-                (externalIsOpen == undefined) ?
-                    <DialogTrigger asChild>
-                        {children}
-                    </DialogTrigger> : null
-            }
+                    {
+                        (externalIsOpen == undefined) ?
+                            <DialogTrigger asChild>
+                                {children}
+                            </DialogTrigger> : null
+                    }
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>{event.name}</DialogTitle>
+                            <DialogDescription>{event.subtitle}</DialogDescription>
+                            <p className="text-sm text-gray-500">Starts at:</p>
+                            <p className="text-sm font-medium text-blue-600">
+                                {format(parseISO(event.date), "PPPpp")}
+                            </p>
+                            <p className="text-sm text-gray-500">Ends at:</p>
+                            <p className="text-sm font-medium text-blue-600">
+                                {format(parseISO(event.endDate), "PPPpp")}
+                            </p>
+                            <Separator></Separator>
+                            <p className="text-l mt-5">{event.description}</p>
+                        </DialogHeader>
+                        <DialogFooter className="flex flex-col sm:flex-col">
+                            {userEvents.some((e) => e.id === event.id) ? (
+                                <Button
+                                    disabled={isButtonDisabled}
+                                    onClick={() => setConfirmAction("return")}
+                                    className="mt-6 px-6 py-3 cursor-pointer bg-primary text-white rounded-lg font-semibold hover:bg-primary/90 transition"
+                                >
+                                    Return ticket
+                                </Button>
+                            ) : (
+                                <Button
+                                    disabled={isButtonDisabled}
+                                    className="mt-6 px-6 py-3 cursor-pointer bg-primary text-white rounded-lg font-semibold hover:bg-primary/90 transition"
+                                    onClick={() => setConfirmAction("buy")}
+                                >
+                                    Buy Ticket
+                                </Button>
+                            )}
+                            <p className="text-sm text-muted-foreground text-center">
+                                {disabledButtonMsg}
+                            </p>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog >
+            </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>{event.name}</DialogTitle>
-                    <DialogDescription>{event.subtitle}</DialogDescription>
-                    <p className="text-sm text-gray-500">Starts at:</p>
-                    <p className="text-sm font-medium text-blue-600">
-                        {format(parseISO(event.date), "PPPpp")}
-                    </p>
-                    <p className="text-sm text-gray-500">Ends at:</p>
-                    <p className="text-sm font-medium text-blue-600">
-                        {format(parseISO(event.endDate), "PPPpp")}
-                    </p>
-                    <Separator></Separator>
-                    <p className="text-l mt-5">{event.description}</p>
+                    <DialogTitle>
+                        {confirmAction === "buy"
+                            ? "Confirm Ticket Purchase"
+                            : "Confirm Ticket Return"}
+                    </DialogTitle>
+                    <DialogDescription>
+                        {confirmAction === "buy"
+                            ? `Are you sure you want to buy a ticket for "${event.name}"?`
+                            : `Are you sure you want to return your ticket for "${event.name}"?`}
+                    </DialogDescription>
                 </DialogHeader>
-                <DialogFooter className="flex flex-col sm:flex-col">
-                    {userEvents.some((e) => e.id === event.id) ? (
-                        <Button
-                            disabled={isButtonDisabled}
-                            onClick={() => setConfirmAction("return")}
-                            className="mt-6 px-6 py-3 cursor-pointer bg-primary text-white rounded-lg font-semibold hover:bg-primary/90 transition"
-                        >
-                            Return ticket
-                        </Button>
-                    ) : (
-                        <Button
-                            disabled={isButtonDisabled}
-                            className="mt-6 px-6 py-3 cursor-pointer bg-primary text-white rounded-lg font-semibold hover:bg-primary/90 transition"
-                            onClick={() => setConfirmAction("buy")}
-                        >
-                            Buy Ticket
-                        </Button>
-                    )}
-                    <p className="text-sm text-muted-foreground text-center">
-                        {disabledButtonMsg}
-                    </p>
+                <DialogFooter className="flex justify-end gap-4">
+                    <Button
+                        variant="outline"
+                        onClick={() => setConfirmAction(null)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button onClick={handleConfirm}>Confirm</Button>
                 </DialogFooter>
             </DialogContent>
-        </Dialog >
+        </Dialog>
+
     );
 }
 
